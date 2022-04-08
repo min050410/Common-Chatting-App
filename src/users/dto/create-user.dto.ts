34d38ebac1b35228bf3e99de3f192bae74ca7ff9
youngmin <min050410@gmail.com>
2022-04-08 21:58:18 +0900

    init: controller

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,6 @@
+// readonly : read only
+export class CreateUserDto {
+    readonly name: string;
+    readonly email: string;
+    readonly password: string;
+}
---
+++ src/users/users.controller.ts
@@ -0,0 +1,16 @@
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UserLoginDto } from './dto/user-login.dto';
+import { VerifyEmailDto } from './dto/verify-email.dto';
+import { UserInfo } from './UserInfo';
+
+// 회원가입 인터페이스 구현
+
+@Controller('users')
+export class UsersController {
+    @Post()
+    async createUser(@Body() dto: CreateUserDto): Promise<void> {
+        console.log(dto);
+    }
+
+}
