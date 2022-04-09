f1a04b4672030f6440311cacd2b0abb6310cabdf
youngmin <min050410@gmail.com>
2022-04-10 02:01:03 +0900

    add: validate

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-// import * as dotenv from 'dotenv';
-// import * as path from 'path';
-
-// dotenv 활용
-
-// dotenv.config({
-//   path: path.resolve(
-//     (process.env.NODE_ENV === 'production') ? '.production.env'
-//     : (process.env.NODE_ENV == 'stage') ? '.stage.env' : '.development.env'
-//   )
-// })
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true,
+  }));
   await app.listen(3000);
 }
 
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,24 @@
+import { BadRequestException } from '@nestjs/common';
+import { Transform } from 'class-transformer';
+import { IsEmail, IsString, Matches, MaxLength, MinLength } from 'class-validator'
+import { NotIn } from 'src/utils/decorators/not-in';
+
 // readonly : read only
 export class CreateUserDto {
+    // 공백 제거 로직
+    @Transform(params => params.value.trim())
+    @NotIn('password', { message: 'password는 name과 같은 문자열을 포함할 수 없습니다.' })
+    @IsString()
+    @MinLength(2)
+    @MaxLength(30)
     readonly name: string;
+
+    @IsString()
+    @IsEmail()
+    @MaxLength(30)
     readonly email: string;
+
+    @IsString()
+    @Matches(/^[A-Za-z\d!@#$%^&*()]{8,30}$/)
     readonly password: string;
 }
