47d6d16bc875aa07cafc40f0b370bf0729a345ee
youngmin <min050410@gmail.com>
2022-04-18 18:00:47 +0900

    fix: board entity

--- src/post/entity/Board.entity.ts
+++ src/post/entity/Board.entity.ts
@@ -1,6 +1,5 @@
 import { Entity, PrimaryColumn, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from "typeorm";
 
-
 @Entity('board')
 export class Board {
 
@@ -10,27 +9,14 @@ export class Board {
     // 외래 키
     user_id: string;
 
-    @Column({length: 40})
-    uuid: string;
     @Column({length: 20})
-    name: string;
-    @Column({length: 100})
-    email: string;
-    @Column({length: 100})
-    password: string;
-
-    @Column("datetime", {
-        name: "last_login_date"
-    })
-    lastLoginDate: Date;
+    title: string;
+    @Column({length: 1000})
+    content: string;
 
     @CreateDateColumn({
         name: "created_at"
     })
     createdDate: Date;
 
-    @UpdateDateColumn({
-        name: "updated_at"
-    })
-    updatedDate: Date;
 }
